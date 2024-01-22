9a56a1a38cfd82d19af74e1b84731f5dfbcfd1b8
hmiftachul11 <hmiftachul11@gmail.com>
2024-01-23 04:46:58 +0700

    Add Frontend React Vite With Tailwind

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,24 @@ import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
-import booksRoute from "./routes/booksRoute.js"
+import booksRoute from "./routes/booksRoute.js";
 
 const app = express();
 
+// Middleawere for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS Policy
+// Option 1: Allow All Origins with Default of Cors(*)
+// app.use(cors());
+// Option 2: Allow Custom Origins
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.get("/", (request, response) => {
   console.log(request);
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+
+
+const App = () => {
+  return (
+    <div className="bg-red-400 text-white">App</div>
+  )
+}
+
+export default App
